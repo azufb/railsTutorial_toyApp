eb754bb0ab97b04275e25cf3c010bc2dd721e0fa
azufb <curiousazunas@gmail.com>
2023-04-06 20:39:20 +0900

    scaffoldでMicropostsリソースを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  # マイクロポスト用URIをMicropostsコントローラ内のアクションに割り当てる
+  resources :microposts
+  # ユーザ用URIをUsersコントローラ内のアクションに割り当てる
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'application#hello'
+  root 'users#index'
 end
