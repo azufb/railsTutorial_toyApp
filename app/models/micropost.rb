12fe7175a1d672229a87b439f679b0b445687ced
azufb <curiousazunas@gmail.com>
2023-04-06 20:44:13 +0900

    バリデーション

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,2 +1,4 @@
 class Micropost < ApplicationRecord
+    # 入力できる文字数を140文字に制限するバリデーション
+    validates :content, length: { maximum: 140 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    # ユーザ名の入力を必須とする
+    validates :name,  presence: true
+    validates :email, presence: true
 end
